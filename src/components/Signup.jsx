a9c81f7d6b17293539c32440ea66b039725b1c5f
jfontecha <jesus.fontecha1@gmail.com>
2020-07-07 11:10:19 +0200

    react-hooks migration and fixing

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,111 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, Container, Row, Col, Card, CardTitle } from 'reactstrap';
 
 import { Link } from 'react-router-dom';
 import { postNewUser } from "../utils/apicalls.js";
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username:'',
-      password:'',
-      fullname:'',
-      email:'',
-      role:'subscriber'
-    };
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleFullnameChange = this.handleFullnameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handleRoleChange = this.handleRoleChange.bind(this);
-    this.onSignup = this.onSignup.bind(this);
-  }
+export default function HeaderApp(props){
 
-  handleUsernameChange(e){
-    this.setState({username:e.target.value})
-  }
-  handlePasswordChange(e){
-    this.setState({password:e.target.value})
-  }
-  handleFullnameChange(e){
-    this.setState({fullname:e.target.value})
-  }
-  handleEmailChange(e){
-    this.setState({email:e.target.value})
-  }
-  handleRoleChange(e){
-    this.setState({role:e.target.value})
-  }
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('subscriber');
 
-  onSignup(e) {
-    const {
-      username,
-      password,
-      fullname,
-      email,
-      role
-    } = this.state;
-
+  const onSignup = (e) => {
     //Post a new user in the database
     postNewUser(username, password, fullname, email, role)
-      .then((res) => this.checkPOSTNewUser(res));
+      .then((res) => checkPOSTNewUser(res));
   }
 
   //Check the response from the server
-  checkPOSTNewUser(res) {
+  const checkPOSTNewUser = (res) => {
     //if response is OK: create a new user and redirect to login page
     if (res === "OK")
-      this.props.history.push("/");
+      props.history.push("/");
     else{
       //TODO Show Modal error adding a new user
     }
   }
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <Card body>
-            <CardTitle tag="h4">Registro de nuevo usuario</CardTitle>
-              <Form>
-                <FormGroup>
-                  <Label for="aUsername">Username</Label>
-                  <Input type="text" name="username" id="aUsername" placeholder="Introduce tu username" onChange={this.handleUsernameChange} required/>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="aPassword">Password</Label>
-                  <Input type="password" name="password" id="aPassword" placeholder="Introduce tu password" onChange={this.handlePasswordChange} required/>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="aFullname">Nombre completo</Label>
-                  <Input type="text" name="fullname" id="aFullname" placeholder="Introduce tu nombre completo" onChange={this.handleFullnameChange} />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="aEmail">Email</Label>
-                  <Input type="email" name="email" id="aEmail" placeholder="Introduce tu email" onChange={this.handleEmailChange} required/>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="aRole">Rol</Label>
-                  <Input type="select" name="role" id="aRole" onChange={this.handleRoleChange}>
-                    <option value="subscriber">Suscriptor</option>
-                    <option value="admin">Administrador</option>
-                  </Input>
-                </FormGroup>
-                <Button onClick={this.onSignup}>Registro</Button>
-              </Form>
-            </Card>
-            <Row>
-              <Col tag="center">
-                <Link to="/"><strong className="text-muted">Login</strong></Link>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <Card body>
+          <CardTitle tag="h4">Registro de nuevo usuario</CardTitle>
+            <Form>
+              <FormGroup>
+                <Label for="aUsername">Username</Label>
+                <Input type="text" name="username" id="aUsername" placeholder="Introduce tu username" onChange={(e) => setUsername(e.target.value)} required/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="aPassword">Password</Label>
+                <Input type="password" name="password" id="aPassword" placeholder="Introduce tu password" onChange={(e) => setPassword(e.target.value)} required/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="aFullname">Nombre completo</Label>
+                <Input type="text" name="fullname" id="aFullname" placeholder="Introduce tu nombre completo" onChange={(e) => setFullname(e.target.value)} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="aEmail">Email</Label>
+                <Input type="email" name="email" id="aEmail" placeholder="Introduce tu email" onChange={(e) => setEmail(e.target.value)} required/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="aRole">Rol</Label>
+                <Input type="select" name="role" id="aRole" onChange={(e) => setRole(e.target.value)}>
+                  <option value="subscriber">Suscriptor</option>
+                  <option value="admin">Administrador</option>
+                </Input>
+              </FormGroup>
+              <Button onClick={onSignup}>Registro</Button>
+            </Form>
+          </Card>
+          <Row>
+            <Col tag="center">
+              <Link to="/"><strong className="text-muted">Login</strong></Link>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 
-export default Signup;
+}
